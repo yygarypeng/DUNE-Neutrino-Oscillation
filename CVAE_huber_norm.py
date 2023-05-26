b45d358571a7c00c8298fed8a9369b4123b28d84
Gary(Mac) <garyphys0915@gapp.nthu.edu.tw>
2023-05-27 01:36:43 +0800

    add the other model from kaggle

--- CVAE_huber_norm.py
+++ CVAE_huber_norm.py
@@ -1,12 +1,9 @@
+# %% [code] {"jupyter":{"outputs_hidden":false}}
 import gc
-import numpy as np
-import pandas as pd
-import scipy
+
 import matplotlib.pyplot as plt
-
-from numba import njit  # acceleration kernel
+import numpy as np
 import tensorflow as tf
-
 
 # check the tensorflow and GPU
 
@@ -19,14 +16,13 @@ print(
 )
 
 
-
 # import the datasets and check the files
 
 import os
 
 # path = "./phys591000-2023-final-project/"  # run on the local machine
 # path = "/content/drive/Shareddrives/2023AI_final/2023AI_final/phys591000-2023-final-project/" # run on the google colab
-path = "/kaggle/input/phys591000-2023-final-project-i/"# Kaggle
+path = "/kaggle/input/dune-neutrino/"  # Kaggle
 if not os.path.isfile(path + "neutrino_test_data.npz") or os.path.isfile(
     path + "neutrino_train_data.npz"
 ):
@@ -40,56 +36,58 @@ data_test = np.load(path + "neutrino_test_data.npz")  # pseudo-exp data
 name_train = data_train.files
 name_test = data_test.files
 
-ve_train, vebar_train, vu_train, vubar_train, theta23_train, delta_train, ldm_train = map(lambda n: data_train[n], name_train)
+(
+    ve_train,
+    vebar_train,
+    vu_train,
+    vubar_train,
+    theta23_train,
+    delta_train,
+    ldm_train,
+) = map(lambda n: data_train[n], name_train)
 ve_test, vebar_test, vu_test, vubar_test = map(lambda n: data_test[n], name_test)
 
 
 # create train and test data
 X_train = np.stack((ve_train, vebar_train, vu_train, vubar_train), axis=-1)
 Y_train = np.stack((theta23_train, delta_train, ldm_train), axis=-1)
-X_test = np.stack((ve_test, vebar_test, vu_test, vubar_test), axis=-1)
-
+# X_test = np.stack((ve_test, vebar_test, vu_test, vubar_test), axis=-1)
 
 from sklearn.linear_model import SGDClassifier
-from sklearn import metrics
 
-train_normal = X_train[ldm_train > 0]
-train_invert = X_train[ldm_train < 0]
-theta23_normal = theta23_train[ldm_train > 0]
-theta23_invert = theta23_train[ldm_train < 0]
-delta_normal = delta_train[ldm_train > 0]
-delta_invert = delta_train[ldm_train < 0]
-
-def norm_alg(input_data):
-    data_max = np.max(input_data,axis = 0)
-    offset = np.min(input_data,axis = 0)
-    scale = data_max - offset
-    input_data  = (input_data-offset)/scale
-    return input_data
-
-#normalize labels to [0,1]
-ldm_train_norm = norm_alg(ldm_train)
-theta23_NH_norm = norm_alg(theta23_normal)
-theta23_IH_norm = norm_alg(theta23_invert)
-delta_NH_norm = norm_alg(delta_normal)
-delta_IH_norm = norm_alg(delta_invert)
-
-#normalize training data to [0,1]
-v_max = np.max(X_train)
-v_min = np.min(X_train)
-train_NH_norm = (train_normal - v_min)/(v_max- v_min)
-train_IH_norm = (train_invert- v_min)/(v_max- v_min)
-labeling = np.where(ldm_train > 0, 1, 0)
-
-# pertrain
-ldm_class = SGDClassifier(max_iter=1000, tol=1e-3)
-ldm_class.fit(X_train[:,:,0],labeling)
-train_predict = ldm_class.predict(X_train[:,:,0])
+# [X] normalize training data to [0,1]
+x_train_NH = X_train[ldm_train > 0]
+print(f"Before normalized, the shape of x_train: {x_train_NH.shape}")
+v_max = np.max(x_train_NH)
+v_min = np.min(x_train_NH)
+print(f"X-train normalized factors (v_max, v_min) = ({v_max}, {v_min})")
+x_train_NH_norm = (x_train_NH - v_min) / (v_max - v_min)
+print(f"After normalized, the shape of x_train: {x_train_NH_norm.shape}")
+# [Y] normalize training label to [0,1]
+y_train_NH = Y_train[:, 0][ldm_train > 0]
+print(f"Before normalized, the shape of y_train: {y_train_NH.shape}")
+v_max = np.max(y_train_NH)
+v_min = np.min(y_train_NH)
+y_train_NH_norm = (y_train_NH - v_min) / (v_max - v_min)
+print(f"Y-train normalized factors (v_max, v_min) = ({v_max}, {v_min})")
+print(f"After normalized, the shape of y_train: {y_train_NH_norm.shape}")
 
 # clear unused variables
-del ldm_class
-del train_normal, train_invert, theta23_normal, theta23_invert, delta_normal, delta_invert
-del ve_train, vebar_train, vu_train, vubar_train, theta23_train, delta_train, ldm_train, ve_test, vebar_test, vu_test, vubar_test
+del (
+    X_train, 
+    Y_train,
+    ve_train,
+    vebar_train,
+    vu_train,
+    vubar_train,
+    theta23_train,
+    delta_train,
+    ldm_train,
+    ve_test,
+    vebar_test,
+    vu_test,
+    vubar_test,
+)
 gc.collect()
 
 # create validation data
@@ -97,28 +95,32 @@ from sklearn.model_selection import train_test_split
 
 # split the training dataset into training and validation, with test_size = 0.2
 tf.random.set_seed(2023)
-x_train, x_val, y_train, y_val = train_test_split(X_train[train_predict == 1], Y_train[:, 0:2][train_predict == 1], test_size=0.2, shuffle=True)
-x_test = X_test
-
-del X_train, Y_train, x_test
-
-
-from tensorflow.keras import Model, Input
-from tensorflow.keras import regularizers
-from tensorflow.keras import backend as K
-from tensorflow.keras.layers import (
-    Reshape,
-    Conv2D,
-    AveragePooling2D,
-    Conv2DTranspose,
-    LeakyReLU,
-    Flatten,
-    Dense,
-    Lambda,
-    Dropout,
-    BatchNormalization,
+x_train, x_val, y_train, y_val = train_test_split(
+    x_train_NH_norm,
+    y_train_NH_norm,
+    test_size=0.2,
+    shuffle=True,
 )
-from tensorflow.keras.layers import Layer
+# clear unused variables
+del x_train_NH_norm, y_train_NH_norm
+
+from tensorflow.keras import Input, Model
+from tensorflow.keras import backend as K
+from tensorflow.keras import regularizers
+from tensorflow.keras.layers import (
+    AveragePooling2D,
+    BatchNormalization,
+    Conv2D,
+    Conv2DTranspose,
+    Dense,
+    Dropout,
+    Flatten,
+    Lambda,
+    Layer,
+    LeakyReLU,
+    Reshape,
+)
+
 
 def create_model_cvae(input_dim, latent_dim):
     # Define sample_z function
@@ -142,7 +144,9 @@ def create_model_cvae(input_dim, latent_dim):
 
         def call(self, inputs):
             mu, log_var = inputs
-            kl_batch = -0.5 * K.sum(1 + log_var - K.square(mu) - K.exp(log_var), axis=-1)
+            kl_batch = -0.5 * K.sum(
+                1 + log_var - K.square(mu) - K.exp(log_var), axis=-1
+            )
             self.add_loss(K.mean(kl_batch), inputs=inputs)
 
             return inputs
@@ -151,26 +155,22 @@ def create_model_cvae(input_dim, latent_dim):
         x = Conv2D(filters, kernel_size=kernel_size, strides=1, padding="same")(inputs)
         x = BatchNormalization()(x)
         x = LeakyReLU(alpha=0.2)(x)
-        x = AveragePooling2D(pool_size=(2, 2))(x)
-        x = Conv2D(filters, kernel_size=kernel_size, strides=1, padding="same")(inputs)
-        x = BatchNormalization()(x)
-        x = LeakyReLU(alpha=0.2)(x)
-        x = AveragePooling2D(pool_size=(2, 2))(x)
-        x = Conv2D(filters, kernel_size=kernel_size, strides=1, padding="same")(inputs)
+        x = Conv2D(filters, kernel_size=kernel_size, strides=1, padding="same")(x)
         x = BatchNormalization()(x)
         x = LeakyReLU(alpha=0.2)(x)
         x = AveragePooling2D(pool_size=(2, 2))(x)
         return x
 
-
     def deconv2d(inputs, filters, kernel_size):
-        x = Conv2DTranspose(filters, kernel_size=kernel_size, strides=1, padding="same")(inputs)
+        x = Conv2DTranspose(
+            filters, kernel_size=kernel_size, strides=1, padding="same"
+        )(inputs)
         x = BatchNormalization()(x)
         x = LeakyReLU(alpha=0.2)(x)
-        x = Conv2DTranspose(filters, kernel_size=kernel_size, strides=1, padding="same")(x)
+        x = Conv2DTranspose(
+            filters, kernel_size=kernel_size, strides=2, padding="same"
+        )(x)
         x = BatchNormalization()(x)
-        x = LeakyReLU(alpha=0.2)(x)
-        x = Conv2DTranspose(1, kernel_size=kernel_size, strides=2, padding="same")(x)
         x = LeakyReLU(alpha=0.2)(x)
         x = Flatten()(x)
         return x
@@ -180,7 +180,7 @@ def create_model_cvae(input_dim, latent_dim):
     x = conv2d(encoder_inputs, filters=64, kernel_size=4)
     x = conv2d(x, filters=32, kernel_size=4)
     x = Flatten()(x)
-    x = Dense(128)(x)
+    x = Dense(256)(x)
     x = LeakyReLU(alpha=0.2)(x)
     x = Dense(latent_dim)(x)
     z_mu = Dense(latent_dim)(x)
@@ -192,13 +192,11 @@ def create_model_cvae(input_dim, latent_dim):
 
     # Decoder
     latent_inputs = Input(shape=(latent_dim,))
-    x = Dense(128)(latent_inputs)
-    x = LeakyReLU(alpha=0.2)(x)
-    x = Dense(256)(x)
+    x = Dense(256)(latent_inputs)
     x = LeakyReLU(alpha=0.2)(x)
     x = Reshape((4, 4, 16))(x)
     x = deconv2d(x, filters=64, kernel_size=4)
-    x = Reshape((8, 8, 1))(x)
+    x = Reshape((16, 16, 16))(x)
     x = deconv2d(x, filters=32, kernel_size=4)
     x = Dense(np.prod(input_dim))(x)
     x = LeakyReLU(alpha=0.2)(x)
@@ -207,13 +205,10 @@ def create_model_cvae(input_dim, latent_dim):
 
     # DNN (conti --> decoder)
     inputs = Input(shape=x.shape)
-    x = Dense(256, kernel_regularizer=regularizers.l2(0.001))(inputs)
-    x = LeakyReLU(alpha=0.2)(x)
-    x = Dense(128, kernel_regularizer=regularizers.l2(0.001))(x)
-    x = LeakyReLU(alpha=0.2)(x)
-    x = Dense(64, kernel_regularizer=regularizers.l2(0.001))(x)
-    x = LeakyReLU(alpha=0.2)(x)
-    x = Dense(1, activation="linear")(x)
+    x = Dense(256, kernel_regularizer=regularizers.l2(0.002), activation="elu")(inputs)
+    x = Dense(64, kernel_regularizer=regularizers.l2(0.002), activation="elu")(x)
+    x = Dense(16, kernel_regularizer=regularizers.l2(0.002), activation="elu")(x)
+    x = Dense(1, activation="relu")(x)
     dnn = Model(inputs, x, name="dnn")
     dnn.summary()
 
@@ -234,7 +229,7 @@ cvae = create_model_cvae(
 )
 
 # Compile the model
-cvae.compile(optimizer=Adam(5e-5), loss="mse")
+cvae.compile(optimizer=Adam(5e-6), loss="huber")
 
 
 from tensorflow.keras.callbacks import EarlyStopping
@@ -251,14 +246,26 @@ early_stopping = EarlyStopping(
 
 cvae.fit(
     x=x_train,
-    y=y_train[:, 0],
-    validation_data=(x_val, y_val[:, 0]),
+    y=y_train,
+    validation_data=(x_val, y_val),
     epochs=256,
-    batch_size=50,
+    batch_size=64,
     callbacks=[early_stopping],
     shuffle=True,
     verbose=2,
 )
 
+# check the loss function
+fig = plt.figure(figsize=(8, 5), dpi=120)
+history = cvae.history.history
+plt.plot(history["loss"], lw=2.5, label="Train", alpha=0.8)
+plt.plot(history["val_loss"], lw=2.5, label="Validation", alpha=0.8)
+plt.title("Epoch vs Huber loss")
+plt.xlabel("epoch")
+plt.ylabel("Loss (Huber)")
+plt.legend(loc="best")
+plt.savefig("CVAE_loss.png")
+plt.close()
+
 # save model
-cvae.save('./CVAE.h5')
+cvae.save("./CVAE_Theta23.h5")
